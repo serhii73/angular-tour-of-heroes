0f20844d1ded886658f2edcdcdee50524e0d7817
Sergey Gorky <sergeygorky@yahoo.com>
2017-09-08 23:06:02 +0300

    update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,19 @@ export class Hero {
 @Component({
   selector: 'my-app',
   template: `
-  <h1>{{title}}</h1>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
-  </div>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div><label>name: </label>{{hero.name}}</div>
-  `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
-
-
 export class AppComponent {
+  title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-  title = 'Tour of Heroes';
 }
