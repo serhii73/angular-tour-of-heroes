c2089d003993ca4b736272127cc03a61a7c17fa8
Sergey Gorky <sergeygorky@yahoo.com>
2017-09-09 10:00:36 +0300

    toh-pt3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule
   ],
   declarations: [
     AppComponent,
