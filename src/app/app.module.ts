6da2c47899f210eb155bc25c6d19b9932cf59e67
Sergey Gorky <sergeygorky@yahoo.com>
2017-09-10 13:32:23 +0300

    Make the router available

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 
-import { RouterModule }   from '@angular/router';
-
-RouterModule.forRoot([
-  {
-    path: 'heroes',
-    component: HeroesComponent
-  }
-])
-
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
